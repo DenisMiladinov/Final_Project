8300102123ff7496ad8f4a7c16013179faa3e88e
DenisMiladinov <denis.miladinov.highschool@buditel.bg>
2025-04-09 17:03:52 +0300

    Added VacationSpotRepository

---
+++ Server/Repositories/VacationSpotRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Models;
+using System.Collections.Generic;
+
+namespace Server.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly ApplicationDbContext _context;
+        protected readonly DbSet<T> _dbSet;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync() => await _dbSet.ToListAsync();
+
+        public async Task<T> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public void Delete(T entity) => _dbSet.Remove(entity);
+
+        public async Task SaveAsync() => await _context.SaveChangesAsync();
+    }
+}

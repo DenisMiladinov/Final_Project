18b99fa69d0240e97555c01c6feae3bd76c94678
DenisMiladinov <denis.miladinov.highschool@buditel.bg>
2025-04-09 17:02:58 +0300

    Added IVacationSpotRepository

--- Server/Repositories/IVacationRepository.cs
+++ Server/Repositories/IVacationRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace Server.Repositories
+﻿using Models;
+
+namespace Server.Repositories
 {
-    public interface IVacationRepository
+    public interface IVacationSpotRepository
     {
+        public interface IVacationSpotRepository : IGenericRepository<VacationSpot>
+        {
+            Task<IEnumerable<VacationSpot>> GetByLocationAsync(string location);
+            Task<IEnumerable<VacationSpot>> GetAvailableSpotsAsync(DateTime startDate, DateTime endDate);
+        }
     }
 }

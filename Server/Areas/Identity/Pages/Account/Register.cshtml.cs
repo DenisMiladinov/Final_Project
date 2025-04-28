5428bb84417df1341d5c7348b721547533ce89ea
DenisMiladinov <denis.miladinov.highschool@buditel.bg>
2025-04-28 14:25:04 +0300

    Styling and Bug Fixes

--- Server/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Server/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -102,7 +102,7 @@ namespace Server.Areas.Identity.Pages.Account
                 var user = CreateUser();
 
                 user.UserName = Input.Username;
-                await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
+                await _userStore.SetUserNameAsync(user, Input.Username, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
 
